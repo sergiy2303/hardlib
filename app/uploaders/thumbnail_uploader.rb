3afcc82a879ab51448dfe1f934b8a37239a03f16
Sergiy Topchiy <sergiy2303@outlook.com>
2016-02-27 23:07:20 +0200

    HL-14 Added whitelist for thumbnail uploader

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -8,4 +8,16 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   version :thumbnail do
     resize_to_fit(50, 50)
   end
+
+  def extension_white_list
+    %w(png jpg jpeg gif)
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    "default_book/" + [version_name, "default.jpg"].compact.join('_')
+  end
 end
