0565a32b8fa17747998459428512d73521e0ff30
Sergiy Topchiy <sergiy2303@outlook.com>
2016-03-09 12:47:28 +0200

    HL-20 Added action show for book

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   expose(:books) { |default| default.display_index(params) }
-
+  expose(:book)
   def access_denied
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :books, only: [:index, :new, :create]
   devise_for :users, controllers: { sessions: "users/sessions", registrations: 'users/registrations' }
+  resources :home, only: :show
   root 'home#index'
   get 'access_denied', to: 'home#access_denied'
   namespace :admin do
