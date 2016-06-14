53ccf47bb6323395f29d51432d59343490c91e6c
Sergiy Topchiy <sergiy2303@outlook.com>
2016-06-14 22:04:17 +0300

    TECH - Migrated into Cloudinary file storage

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 class AttachmentUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
